6b6fb7ac7a4c6a2ae1adadca22c6792b5c48034c
Keoni Murray <keonimurray45@gmail.com>
2019-03-07 00:45:35 -0800

    build cli

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
-
-import "key-extract/extract"
+import (
+	"bufio"
+	"fmt"
+	"key-extract/extract"
+	"os"
+	"os/exec"
+	"strings"
+)
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		fmt.Print("$Keyextract ")
+		cmdString, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		err = runCommand(cmdString)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
+}
+func runCommand(commandStr string) error {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
 
-	extract.GetLink("https://stackoverflow.com/questions/37218930/why-cant-go-build-from-absolute-path")
+	case "start":
+		fmt.Println("Welcome to key extract please enter the command extract followed by the link")
+	case "extract":
+		fmt.Println("***THIS IS ARGS:***", arrCommandStr[1:])
+		fmt.Println("extracting site....")
+		link := arrCommandStr[1:]
+		result := strings.Join(link, "")
+		fmt.Println("data extracted")
+		extract.GetLink(result)
+	case "help":
+		fmt.Println("")
 
+
+	}
+
+	cmd := exec.Command(arrCommandStr[0], arrCommandStr[1:]...)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	return cmd.Run()
 }
